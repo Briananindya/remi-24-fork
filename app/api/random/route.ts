62056561ddedd5257e0b68b884d32597e786593e
fairuz <this.fairuz@gmail.com>
2023-12-20 08:37:43 +0700

    Fix type error (1)

--- app/api/random/route.ts
+++ app/api/random/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request) {
+export async function POST(request: NextRequest) {
     // Generate an array of 4 random integers
     let randomArray: number[] = [];
     for (let i = 0; i < 4; i++) {
